d6ec0853bfff6d8221595a3cf8182b970840472f
IgnAlbert <albertoojeropuebla@gmail.com>
2022-09-25 13:02:07 +0200

    Hello y renombrar variables

--- src/main/java/com/example/spaceapps/HelloController.java
+++ src/main/java/com/example/spaceapps/HelloController.java
@@ -1,14 +1,40 @@
 package com.example.spaceapps;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button bBegin;
+
+    @FXML
+    private Button bContinue;
+
+    @FXML
+    private HBox fondo;
+
+    @FXML
+    private Label lLoad;
+
+    @FXML
+    private Label lNew;
+
+    @FXML
+    private VBox marcoNombre;
+
+    @FXML
+    void pressBegin(ActionEvent event) {
+
     }
+
+    @FXML
+    void pressContinue(ActionEvent event) {
+
+    }
+
 }
