e6729e29048a76ed3af1894f4b0843ce7c5f2e4b
yaaaiii <yaidelatorre@gmail.com>
2022-09-28 13:52:05 +0200

    algo2

--- src/main/java/com/example/spaceapps/controllerOpciones.java
+++ src/main/java/com/example/spaceapps/controllerOpciones.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.stage.Stage;
@@ -13,16 +14,19 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static javafx.scene.paint.Color.DARKSEAGREEN;
+import static javafx.scene.paint.Color.RED;
+
 public class controllerOpciones {
-    public void initialize(URL url, ResourceBundle rb){
 
-
-    }
     @FXML
     private RadioButton opcion1;
 
     @FXML
     private RadioButton opcion2;
+
+    @FXML
+    private Button compr;
 
     @FXML
     private RadioButton opcion3;
@@ -32,6 +36,19 @@ public class controllerOpciones {
 
     @FXML
     private Label pregunta;
+
+    @FXML
+    private Label progreso;
+    int contador;
+    int p;
+    int r;
+    Juego juego = new Juego();
+    boolean resp1;
+    boolean resp2;
+    boolean resp3;
+    boolean resp4;
+    Respuesta respuestaSeleccionada;
+    Respuesta respuestaCorrecta;
 
     @FXML
     void botonAceptar(ActionEvent event) {
@@ -53,27 +70,45 @@ public class controllerOpciones {
 
     @FXML
     void botonResp1(ActionEvent event) {
-
+        opcion1.setSelected(true);
+        opcion2.setSelected(false);
+        opcion3.setSelected(false);
+        opcion4.setSelected(false);
+        respuestaSeleccionada = juego.getPreguntas().get(p).getListaOpciones().get(p);
+        r=1;
     }
 
     @FXML
     void botonResp2(ActionEvent event) {
-
+        opcion1.setSelected(false);
+        opcion2.setSelected(true);
+        opcion3.setSelected(false);
+        opcion4.setSelected(false);
+        respuestaSeleccionada = juego.getPreguntas().get(p).getListaOpciones().get(p+1);
+        r=2;
     }
 
     @FXML
     void botonResp3(ActionEvent event) {
-
+        opcion1.setSelected(false);
+        opcion2.setSelected(false);
+        opcion3.setSelected(true);
+        opcion4.setSelected(false);
+        respuestaSeleccionada = juego.getPreguntas().get(p).getListaOpciones().get(p+2);
+        r=3;
     }
 
     @FXML
     void botonResp4(ActionEvent event) {
-
+        opcion1.setSelected(false);
+        opcion2.setSelected(false);
+        opcion3.setSelected(false);
+        opcion4.setSelected(true);
+        respuestaSeleccionada = juego.getPreguntas().get(p).getListaOpciones().get(p+3);
+        r=4;
     }
     public void setearPreguntas(int n){
-        int p = n;
-        Juego juego = new Juego();
-        OpcionesMultiples pregunta2 = new OpcionesMultiples();
+         p = n;
         pregunta.setText(juego.getPreguntas().get(p).getCuerpoPregunta().toString() );
 
 
@@ -81,11 +116,47 @@ public class controllerOpciones {
         PERO IRIA DENTRO DEL SET TEXT.
         esto era de prueba xd:)))
         */
-        opcion1.setText("1");
-        opcion2.setText("2");
-        opcion3.setText("3");
-        opcion4.setText("4");
+        opcion1.setText(juego.getPreguntas().get(p).getListaOpciones().get(p).getCuerpoRespuesta());
+        resp1= juego.getPreguntas().get(p).getListaOpciones().get(p).isCorrecta();
+        opcion2.setText(juego.getPreguntas().get(p).getListaOpciones().get(p+1).getCuerpoRespuesta());
+        resp2= juego.getPreguntas().get(p).getListaOpciones().get(p+1).isCorrecta();
+        opcion3.setText(juego.getPreguntas().get(p).getListaOpciones().get(p+2).getCuerpoRespuesta());
+        resp3= juego.getPreguntas().get(p).getListaOpciones().get(p+2).isCorrecta();
+        opcion4.setText(juego.getPreguntas().get(p).getListaOpciones().get(p+3).getCuerpoRespuesta());
+        resp4= juego.getPreguntas().get(p).getListaOpciones().get(p+3).isCorrecta();
+
 
     }
+    @FXML
+    void botonComprobar(ActionEvent event) {
+
+        if (resp1) opcion1.setTextFill(DARKSEAGREEN);
+        if (resp2) opcion2.setTextFill(DARKSEAGREEN);
+        if (resp3) opcion3.setTextFill(DARKSEAGREEN);
+        if (resp4) opcion4.setTextFill(DARKSEAGREEN);
+
+        if(!respuestaSeleccionada.isCorrecta()){
+
+            if(r==1) opcion1.setTextFill(RED);
+
+            if(r==2) opcion2.setTextFill(RED);
+
+            if(r==3) opcion3.setTextFill(RED);
+
+            if(r==4) opcion4.setTextFill(RED);
+         }
+        opcion1.setDisable(true);
+        opcion2.setDisable(true);
+        opcion3.setDisable(true);
+        opcion4.setDisable(true);
+        compr.setDisable(true);
+    }
+
+
+
 }
+
+
+
+
 
