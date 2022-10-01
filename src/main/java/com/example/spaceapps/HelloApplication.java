ac125754b89779c5490afaa22efed01b9d767445
yaaaiii <yaidelatorre@gmail.com>
2022-10-01 13:16:57 +0200

    algo2

--- src/main/java/com/example/spaceapps/HelloApplication.java
+++ src/main/java/com/example/spaceapps/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("indice_y_nombre.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        Scene scene = new Scene(fxmlLoader. load());
         stage.setTitle("Hello!");
         stage.setMaximized(false);
         stage.setResizable(false);
