93e9f122ba3fdf854e5239d0e690f76e74d39b4a
yaaaiii <yaidelatorre@gmail.com>
2022-10-01 14:11:12 +0200

    algo2

--- src/main/java/com/example/spaceapps/nombreController.java
+++ src/main/java/com/example/spaceapps/nombreController.java
@@ -7,74 +7,64 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloController {
-
-    @FXML
-    private Button bBegin;
+public class nombreController {
+    private OpcionesJuego opciones;
     @FXML
     private Label errornombre;
-
-    @FXML
-    private TextField nombre;
-    @FXML
-    private VBox fondo;
     @FXML
     private Button bContinue;
-
-
-
-    @FXML
-    private Label lLoad;
 
     @FXML
     private Label lNew;
 
     @FXML
+    private Label lNew1;
+
+    @FXML
     private VBox marcoNombre;
 
     @FXML
-    void pressBegin(ActionEvent event) throws IOException {
+    private TextField nombre;
+
+    @FXML
+    void pressContinue(ActionEvent event) throws IOException {
         if(!nombre.getText().isBlank()) {
             errornombre.visibleProperty().set(false);
-            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("new_preuba.fxml"));
-            Scene scene = new Scene(fxmlLoader.load());
             OpcionesJuego opciones = new OpcionesJuego();
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("nivel1.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
             Stage stage = new Stage();
-            Stage stage1 = (Stage) bBegin.getScene().getWindow();
-            nombreController controlador = fxmlLoader.getController();
+            Stage stage1 = (Stage) bContinue.getScene().getWindow();
+            nivel1Controller controlador = fxmlLoader.getController();
+            controlador.ponerNombre(nombre.getText());
             opciones.crearJuego();
+            opciones.juego.setNombre(nombre.getText());
             controlador.setOpciones(opciones);
             stage1.close();
-            stage.setTitle("Level 2");
+            stage.setTitle("Level 1");
             stage.setMaximized(false);
             stage.setResizable(false);
             stage.setScene(scene);
             stage.show();
         }
-        else{
+        else {
             errornombre.visibleProperty().set(true);
             errornombre.setText("You must introduce a valid name");
-        }
+        }}}
+
+    public OpcionesJuego getOpciones() {
+        return opciones;
     }
 
-    @FXML
-    void pressContinue(ActionEvent event) throws IOException, ClassNotFoundException {
-
-        OpcionesJuego opciones = new OpcionesJuego();
-        try {
-            opciones.juego = opciones.Cargar("juego.bin");
-        } catch (Exception ex){
-            ///Aqui va el codigo para que informe  que no puedes hacer eso
-            System.out.println("No puedes cargar payaso");
-        }
-
+    public void setOpciones(OpcionesJuego opciones) {
+        this.opciones = opciones;
+    }
 
     }
 
-}
+
