2cd08ac3b1f6218139ca5abdfb98568dcf4630f6
yaaaiii <yaidelatorre@gmail.com>
2022-10-01 10:09:34 +0200

    algo2

--- src/main/java/com/example/spaceapps/nivel3Controller.java
+++ src/main/java/com/example/spaceapps/nivel3Controller.java
@@ -2,10 +2,15 @@ package com.example.spaceapps;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class nivel3Controller {
 
@@ -33,7 +38,18 @@ public class nivel3Controller {
     }
 
     @FXML
-    void IrANivel4(ActionEvent event) {
+    void IrANivel4(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("nivel4.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = new Stage();
+        Stage stage1 = (Stage) bUrano.getScene().getWindow();
+       // nivel2Controller controlador = fxmlLoader.getController();
+        stage1.close();
+        stage.setTitle("Level 4");
+        stage.setMaximized(false);
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
 
     }
 
