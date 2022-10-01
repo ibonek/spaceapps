b837e8bbcc2f4075997364e030392e822f404c1f
IgnAlbert <albertoojeropuebla@gmail.com>
2022-10-01 15:42:33 +0200

    Seleccion niveles

---
+++ src/main/java/com/example/spaceapps/SelectorController.java
@@ -0,0 +1,56 @@
+package com.example.spaceapps;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+public class SelectorController {
+
+    @FXML
+    private Button bLevel1;
+
+    @FXML
+    private Button bLevel2;
+
+    @FXML
+    private Button bLevel3;
+
+    @FXML
+    private Button bLevel4;
+
+    @FXML
+    private Button bLevel5;
+
+    @FXML
+    private Label eNombre;
+
+    @FXML
+    private HBox fondoSelect;
+
+    @FXML
+    void BotonLevel5(ActionEvent event) {
+
+    }
+
+    @FXML
+    void botonLevel1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void botonLevel2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void botonLevel3(ActionEvent event) {
+
+    }
+
+    @FXML
+    void botonLevvel4(ActionEvent event) {
+
+    }
+
+}
